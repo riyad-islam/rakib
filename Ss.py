0474aff95ac2b49cd023f6ba23e61e990a0beeb5
riyad-islam <122849784+riyad-islam@users.noreply.github.com>
2023-03-26 00:20:47 +0600

    Hi.py

--- Ss.py
+++ Ss.py
@@ -1,4 +1,4 @@
 import os
 os.system('pkg install espeak')
 print('Wolcame to mehedi tools')
-os.system('espeak -a 300" welcome to Mehedi free tools')
+os.system('espeak -a 300 "welcome to Mehedi free tools')
